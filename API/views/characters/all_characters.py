8b9c8c36c7935944b0be5219d3fdeb8b00578c5c
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-09-15 14:58:21 +0100

    get characters endpoint

--- API/views/characters/all_characters.py
+++ API/views/characters/all_characters.py
@@ -1,4 +1,6 @@
 import requests
+import json
+from django.http import JsonResponse, HttpResponse
 from rest_framework import views, status, permissions
 from rest_framework.response import Response
 
@@ -9,6 +11,7 @@ from API.lib.headers import request_headers, BASE_URL
 class GetCharacters(views.APIView):
     # permission_classes = [permissions.IsAuthenticated]
     def get(self, request):
-        api_request = requests.get(BASE_URL+'characters', headers=request_headers())
-        print(api_request)
-        return Response({"Onaga":"Onaga"})
+        api_request = requests.get(BASE_URL+'character', headers=request_headers())
+        data = json.dumps(api_request.json())
+        return HttpResponse(data, content_type='application/json')
+
